a6967af5b44bd9d4d66770647cd7571a8ad9262d
Bastyan Farias <bastyfarias30@gmail.com>
2025-04-15 00:07:00 -0400

    Ahora funcionan las categorias

--- js/Categorias.js
+++ js/Categorias.js
@@ -4,10 +4,74 @@
     const catalogo = document.querySelector('.Catalogo');
     let currentPage = 1;
     
-    switch(categoria)
+    function cargarmangas()
     {
-        case 'populares':
-            fetch(`https://api.jikan.moe/v4/top/manga?page=${page}&limit=25`)   
-        break;
+        catalogo.innerHTML='';
+        switch(categoria)
+        {
+            case 'populares':
+                
+
+                fetch(`https://api.jikan.moe/v4/top/manga?page=${currentPage}&limit=25`)
+                    .then(response => response.json())
+                    .then(data=> {
+                        data.data.forEach(manga=> {
+                            const Popular = document.createElement('div')
+                            Popular.classList.add('Popular')
+                            Popular.innerHTML = `
+                                <a href="${manga.url}" target="_blank">
+                                    <img src="${manga.images.jpg.image_url}" alt="${manga.title}" />
+                                    <p>${manga.title}</p>
+                                </a>
+                                `;
+                                catalogo.appendChild(Popular);
+                        });
+                    })
+            break;
+
+            case 'Publishing':
+            case 'Complete':
+                fetch(`https://api.jikan.moe/v4/manga?status=${categoria}&limit=25`)
+                    .then(response => response.json())
+                    .then(data => 
+                        {
+                        data.data.forEach(manga => 
+                            {
+                            const Popular = document.createElement('div')
+                            Popular.classList.add('Popular')
+                            Popular.innerHTML = `
+                                <a href="${manga.url}" target="_blank">
+                                    <img src="${manga.images.jpg.image_url}" alt="${manga.title}" />
+                                    <p>${manga.title}</p>
+                                </a>
+                            `;
+                            catalogo.appendChild(Popular);
+                            });
+                        });
+            break;
+            
+            default:
+                fetch(`https://api.jikan.moe/v4/manga?genres=${categoria}&limit=25`)
+                    .then(response => response.json())
+                    .then(data => 
+                        {
+                        data.data.forEach(manga => 
+                            {
+                            const Popular = document.createElement('div')
+                            Popular.classList.add('Popular')
+                            Popular.innerHTML = `
+                                <a href="${manga.url}" target="_blank">
+                                    <img src="${manga.images.jpg.image_url}" alt="${manga.title}" />
+                                    <p>${manga.title}</p>
+                                </a>
+                            `;
+                            catalogo.appendChild(Popular);
+                            });
+                        });
+            break;
+        }
     }
+    
+    cargarmangas();
+    
 
