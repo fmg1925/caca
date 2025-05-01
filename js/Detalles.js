7077f40fc066a082a9b6879f61fc17bc67e41a68
spectro451 <bastyfarias30@gmail.com>
2025-05-01 01:29:18 -0400

    Se añadieron datos a Detalles

--- js/Detalles.js
+++ js/Detalles.js
@@ -11,6 +11,7 @@ if (idManga) {
         .then(response => response.json())
         .then(data => {
             const manga = data.data;
+            const relaciones = manga.relations || [];
             const img = document.createElement('img');
             const ranking = document.getElementById('ranking');
             const sinopsis = document.getElementById('sinopsis');
@@ -19,7 +20,12 @@ if (idManga) {
             const foro = document.getElementById('foro');
             const fechaInicio = new Date(manga.published?.from).toLocaleDateString();
             const fechaFin = manga.published?.to ? new Date(manga.published.to).toLocaleDateString() : 'hasta hoy';
-            
+            let sinopsisLimpia = manga.synopsis.replace(/\[Written by MAL Rewrite\]/, '');
+            let sinopsisFormateada = sinopsisLimpia
+            .split('\n\n') // separamos por saltos dobles de línea
+            .map(parrafo => `<p>${parrafo.trim()}</p>`)
+            .join('');
+
             img.src=manga.images.jpg.image_url;
             img.alt=manga.title;
             
@@ -34,23 +40,79 @@ if (idManga) {
             </ul>
             <hr>
             <h4>Información</h4>
-            
-            <p><strong>tipo:</strong> ${manga.type}</p>
+            <p><strong>Tipo:</strong> ${manga.type}</p>
             <p><strong>Categoria:</strong> ${(manga.demographics?.length > 0 ? manga.demographics.map(categ => categ.name).join(', ') : 'Desconocida')}</p>
+            <p><strong>Genero/s:</strong> ${(manga.genres?.length > 0 ? manga.genres.map(gen => gen.name).join(', ') : 'Desconocida')}</p>
+            <p><strong>Tema/s:</strong> ${(manga.themes?.length > 0 ? manga.themes.map(tem => tem.name).join(', ') : 'Desconocida')}</p>
             <p><strong>Autor:</strong> ${(manga.authors?.length > 0 ? manga.authors.map(autor => autor.name).join(', ') : 'Desconocido')}</p>
             <p><strong>Capítulos:</strong> ${manga.chapters || 'Desconocida'}</p>
-            <p><strong>volumenes:</strong> ${manga.volumes || 'Desconocida'}</p>
+            <p><strong>Volumenes:</strong> ${manga.volumes || 'Desconocida'}</p>
             <p><strong>Estado:</strong> ${manga.status || 'Desconocida'}</p>
+            <p><strong>Editorial:</strong> ${(manga.serializations?.length > 0 ? manga.serializations.map(peo => peo.name).join(', ') : 'Desconocido')}</p>
             <p><strong>Fecha de publicación:</strong> ${fechaInicio} - ${fechaFin}</p>
-            
             <hr>
             `;
             estadisticas.innerHTML =
             `
-            <p><strong>Puntuacion:</strong> ${manga.score || 'Desconocida'} (reseñado por: ${manga.scored_by || 'Desconocida'})</p>
+            <p><strong>Puntuacion:</strong> ${manga.score || 'Desconocida'} (reseñado por <strong>${manga.scored_by || 'Desconocida'}</strong> usuarios)</p>
             <p><strong>Ranking:</strong> #${manga.rank || 'Desconocida'}</p>
             <p><strong>Rankig usuarios:</strong> #${manga.popularity || 'Desconocida'}</p>
             <p><strong>Favorito de:</strong> ${manga.favorites || 'Desconocida'}</p>
+            
+            `;
+            ranking.innerHTML =
+            `
+            <strong>Puntuacion</strong> 
+            <p>${manga.score || 'Desconocida'} (reseñado por <strong>${manga.scored_by || 'Desconocida'}</strong> usuarios)</p>
+            <p><strong>Ranking:</strong> #${manga.rank || 'Desconocida'}</p>
+            <p><strong>Rankig Popular:</strong> #${manga.popularity || 'Desconocida'}</p>
+            <p><strong>Favorito de:</strong> ${manga.favorites || 'Desconocida'}</p>
+            <hr>
+            `;
+            sinopsis.innerHTML =
+            `
+            <strong>Sinopsis</strong> 
+            <p>${sinopsisFormateada || 'Desconocida'}</p>
+            <hr>
+            `;
+            relacionados.innerHTML =
+            `
+            <strong>Relacionado</strong> 
+            <p>
+                ${relaciones?.length > 0 ? relaciones
+                        .map(rel => `${rel.relation}: ${rel.entry.map(e => e.name).join(', ')}`)
+                        .join('<br>')
+                    : 'Desconocida'
+                }
+            </p>
+
+            <hr>
+            `;
+            fetch(`https://api.jikan.moe/v4/manga/${manga.mal_id}/characters`)
+            .then(res => res.json())
+            .then(data => {
+              data.data.forEach(personaje => {
+                const divPersonaje = document.createElement('div');
+                divPersonaje.innerHTML = 
+                `
+                <a href="Personajes.html?id=${personaje.character.mal_id}">
+                    <img src="${personaje.character.images.jpg.image_url}" alt="${personaje.character.name}" />
+                    <p>${personaje.character.name}</p>
+                    <p>Rol: ${personaje.role}</p>
+                </a>
+                `;
+                personajes.appendChild(divPersonaje);
+              });
+            });
+            foro.innerHTML =
+            `
+            <hr>
+            <strong>Puntuacion</strong> 
+            <p>${manga.score || 'Desconocida'} (reseñado por <strong>${manga.scored_by || 'Desconocida'}</strong> usuarios)</p>
+            <p><strong>Ranking:</strong> ${manga.rank || 'Desconocida'}</p>
+            <p><strong>Rankig Popular:</strong> ${manga.popularity || 'Desconocida'}</p>
+            <p><strong>Favorito de:</strong> ${manga.favorites || 'Desconocida'}</p>
+            <hr>
             `;
 
             imagenManga.appendChild(img);
